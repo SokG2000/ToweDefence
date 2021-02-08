ce70f74a3356e41ffcb4a0a9c6020c8faeb86248
sokolovgm <sokolov.gm@phyctech.edu>
2021-02-08 20:45:43 +0300

    Тест коммит

---
+++ Assets/Scripts/MovementAgent.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAgent : MonoBehaviour
+{
+    [SerializeField]
+    private float m_Speed;
+    [SerializeField]
+    private Vector3 m_Target;
+
+    private const float TOLERANCE = 0.1f;
+    void Start()
+    {
+        m_Speed = 5f;
+        m_Target = new Vector3(10, 0, 10);
+    }
+
+    void Update()
+    {
+        float distance = (m_Target - transform.position).magnitude;
+        if (distance < TOLERANCE)
+        {
+            return;
+        }
+        Vector3 dir = m_Target - transform.position;
+        Vector3 delta = dir * (m_Speed * Time.deltaTime);
+        transform.Translate(delta);
+    }
+}
