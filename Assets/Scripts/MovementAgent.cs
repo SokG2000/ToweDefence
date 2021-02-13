dafe555052376d325d8dfac666761dcfa2d397d0
sokolovgm <sokolov.gm@phyctech.edu>
2021-02-13 19:04:30 +0300

    Исправления к первому домашнему заданию.

--- Assets/Scripts/MovementAgent.cs
+++ Assets/Scripts/MovementAgent.cs
@@ -15,19 +15,20 @@ public class MovementAgent : MonoBehaviour
     [SerializeField]
     private float G;
     [SerializeField]
-    private float _Mass;
-    [SerializeField]
     private float _TargetMass;
     private const float TOLERANCE = 0.1f;
+    [SerializeField]
+    private GameObject _Target;
     
-    private Camera cam;
+    //private Camera cam;
 
     
     void Start()
     {
         transform.position = _StartPosition;
-        cam = GameObject.Find("Main Camera").GetComponent("Camera") as Camera;
+        //cam = Camera.main;
         _Speed = _StartSpeed;
+        _Target.transform.position = TargetPosition;
     }
 
     void FixedUpdate()
@@ -37,29 +38,10 @@ public class MovementAgent : MonoBehaviour
         {
             return;
         }
-        
-        Vector3 viewPos = cam.WorldToViewportPoint(transform.position);
-        var x = viewPos.x;
-        if ((x < 0 && _Speed.x < 0) || (x > 1 && _Speed.x > 0))
-        {
-            _Speed.x = -_Speed.x;
-        }
-        var y = viewPos.y;
-        if ((y < 0 && _Speed.y < 0) || (y > 1 && _Speed.y > 0))
-        {
-            _Speed.y = -_Speed.y;
-        }
-        var z = viewPos.z;
-        if (z < 0 && _Speed.z < 0)
-        {
-            _Speed.z = -_Speed.z;
-        }
-
         Vector3 a = (TargetPosition - transform.position).normalized * (G * _TargetMass /
                                                                         (distance * distance));
         transform.position = transform.position + _Speed * Time.fixedDeltaTime +
-                             a * (Time.fixedDeltaTime * Time.fixedDeltaTime);
+                             a * (Time.fixedDeltaTime * Time.fixedDeltaTime / 2);
         _Speed = _Speed + a * Time.fixedDeltaTime;
-        
     }
 }
