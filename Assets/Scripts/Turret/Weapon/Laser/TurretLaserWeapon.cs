0bd120bcb22974620102cbb927c9c0e7f0ec46a5
sokolovgm <sokolov.gm@phyctech.edu>
2021-03-31 19:46:37 +0300

    Визуализация лазера

--- Assets/Scripts/Turret/Weapon/Laser/TurretLaserWeapon.cs
+++ Assets/Scripts/Turret/Weapon/Laser/TurretLaserWeapon.cs
@@ -7,17 +7,16 @@ using UnityEngine;
 
 namespace Turret.Weapon.Laser
 {
-    public class TurretLaserWeapon: ITurretWeapon
+    public class TurretLaserWeapon : ITurretWeapon
     {
         private TurretLaserWeaponAsset m_Asset;
         private TurretView m_View;
-        private LineRenderer m_LineRenderer;        
-        [CanBeNull]
-        private EnemyData m_ClosestEnemyData;
-        
+        private LineRenderer m_LineRenderer;
+        [CanBeNull] private EnemyData m_ClosestEnemyData;
+
         private float m_DamagePerSecond;
         private float m_MaxDistance;
-        
+
         private List<Node> AccessibleNodes;
 
         public TurretLaserWeapon(TurretLaserWeaponAsset asset, TurretView view)
@@ -27,30 +26,50 @@ namespace Turret.Weapon.Laser
             m_DamagePerSecond = asset.DamagePerSecond;
             m_MaxDistance = asset.MaxDistance;
             AccessibleNodes = Game.Player.Grid.GetNodesInCircle(m_View.ProjectileOrigin.position, m_MaxDistance);
-            //m_LineRenderer = Object.Instantiate(asset.LineRendererPrefab, m_View);
+            m_LineRenderer =
+                Object.Instantiate(asset.LineRendererPrefab, m_View.transform.position, Quaternion.identity);
+            m_LineRenderer.SetPosition(0, Vector3.zero);
+            m_LineRenderer.SetPosition(1, Vector3.forward);
+            m_LineRenderer.gameObject.SetActive(false);
         }
 
         public void TickShoot()
         {
-            m_ClosestEnemyData = EnemySearch.GetClosestEnemy(AccessibleNodes, m_View.transform.position, m_MaxDistance);
+            m_ClosestEnemyData =
+                EnemySearch.GetClosestEnemy(AccessibleNodes, m_View.ProjectileOrigin.position, m_MaxDistance);
+
+            TickTower();
 
             if (m_ClosestEnemyData == null)
             {
                 return;
             }
-            
-            TickTower();
-            
+
             m_ClosestEnemyData.GetDamage(m_DamagePerSecond * Time.deltaTime);
         }
-        
+
         private void TickTower()
         {
             if (m_ClosestEnemyData != null)
             {
                 m_View.TowerLookAt(m_ClosestEnemyData.MView.transform.position);
             }
+
+            TickLaser();
         }
 
+        private void TickLaser()
+        {
+            if (m_ClosestEnemyData == null)
+            {
+                m_LineRenderer.gameObject.SetActive(false);
+                return;
+            }
+
+            Vector3 originPosition = m_View.ProjectileOrigin.position;
+            m_LineRenderer.transform.position = originPosition;
+            m_LineRenderer.SetPosition(1, m_ClosestEnemyData.MView.transform.position - originPosition);
+            m_LineRenderer.gameObject.SetActive(true);
+        }
     }
 }
